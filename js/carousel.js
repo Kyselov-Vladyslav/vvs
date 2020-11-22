67922bba261ed2ac66cb62435f73e286f6bbdc15
Kyselov-Vladyslav <66912506+Kyselov-Vladyslav@users.noreply.github.com>
2020-11-22 13:40:44 +0100

    ООО НПФ « В.В.С.»

---
+++ js/carousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#carousel-header").owlCarousel({
+        items: 1,
+        nav: true,
+        navText: [],
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true
+        
+    });
+    
+  });
